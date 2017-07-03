053af31bfd33c5e288b204b542cb338c1e592d33
LaryRabbit <black.rabbit@outlook.es>
2017-07-02 19:39:11 -0500

    Branch ejemplos
    Preferí crear una rama para que subamos puros ejemplos (si es que llegamos a ocupar más). Así en la principal solo se haran commits que afecten el código de la aplicación.

--- DarkRoom/openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ DarkRoom/openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\Nathaniel\\Desktop\\DarkRoom_modificable\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\Larissa\\Documents\\GitHub\\TT\\DarkRoom\\openCVLibrary320\\src\\main\\aidl\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
