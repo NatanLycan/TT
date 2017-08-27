1efeb0b75ed640fc99057e990db7a2eae8176e08
NatanLycan <nathaniel981@gmail.com>
2017-08-27 17:02:48 -0500

    ColorBlob ver Alt OpenCV 3.3.0
    Migracion de OpenCV 2.4.12 -> 3.3.0
    Para funciones de drawing

---
+++ color-blob-detection-verAlt-3.3.0/openCVSamplecolorblobdetection/src/main/java/org/opencv/samples/colorblobdetect/ColorBlobDetectionActivity.java
@@ -0,0 +1,404 @@
+package org.opencv.samples.colorblobdetect;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.Serializable;
+import java.util.List;
+
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.android.Utils;
+import org.opencv.core.Core;
+import org.opencv.core.CvException;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Rect;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
+import org.opencv.imgproc.Imgproc;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.View.OnTouchListener;
+import android.view.SurfaceView;
+
+import static java.lang.System.exit;
+
+public class ColorBlobDetectionActivity extends Activity implements OnTouchListener, CvCameraViewListener2 {
+    private static final String  TAG              = "OCVSample::Activity";
+
+    private boolean              mIsColorSelected = false;
+    private Mat                  mRgba;
+    private Mat                  pp_mRgba_original;
+    private Scalar               mBlobColorRgba;
+    private Scalar               mBlobColorHsv;
+    private ColorBlobDetector    mDetector;
+    private Mat                  mSpectrum;
+    private Size                 SPECTRUM_SIZE;
+    private Scalar               CONTOUR_COLOR;
+    private  String              pp_imgAdd = "Dirección por defecto";
+    private  String              pp_imgAdd2 = "Dirección por defecto 2";
+
+    private CameraBridgeViewBase mOpenCvCameraView;
+
+    private BaseLoaderCallback  mLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS:
+                {
+                    Log.i(TAG, "OpenCV loaded successfully");
+                    mOpenCvCameraView.enableView();
+                    mOpenCvCameraView.setOnTouchListener(ColorBlobDetectionActivity.this);
+                } break;
+                default:
+                {
+                    super.onManagerConnected(status);
+                } break;
+            }
+        }
+    };
+
+    public ColorBlobDetectionActivity() {
+        Log.i(TAG, "Instantiated new " + this.getClass());
+    }
+
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        Log.i(TAG, "called onCreate");
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+
+        setContentView(R.layout.color_blob_detection_surface_view);
+
+        mOpenCvCameraView = (CameraBridgeViewBase) findViewById(R.id.color_blob_detection_activity_surface_view);
+        mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
+        mOpenCvCameraView.setCvCameraViewListener(this);
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    @Override
+    public void onResume()
+    {
+        super.onResume();
+        if (!OpenCVLoader.initDebug()) {
+            Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_0_0, this, mLoaderCallback);
+        } else {
+            Log.d(TAG, "OpenCV library found inside package. Using it!");
+            mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
+        }
+    }
+
+    public void onDestroy() {
+        super.onDestroy();
+        if (mOpenCvCameraView != null)
+            mOpenCvCameraView.disableView();
+    }
+
+    public void onCameraViewStarted(int width, int height) {
+        mRgba = new Mat(height, width, CvType.CV_8UC4);
+        mDetector = new ColorBlobDetector();
+        mSpectrum = new Mat();
+        mBlobColorRgba = new Scalar(255);
+        mBlobColorHsv = new Scalar(255);
+        SPECTRUM_SIZE = new Size(200, 64);
+        CONTOUR_COLOR = new Scalar(255,0,0,255);
+    }
+
+    public void onCameraViewStopped() {
+        mRgba.release();
+    }
+
+    public boolean onTouch(View v, MotionEvent event) {
+        /**
+         * PP NCH 30/07/2017
+         * Se obtendran tres eventos
+         * 1- EL primer touch debera ser el del Objeto a medir y cambiara el color de contorno a verde
+         * 2- El segundo toque sera al Objeto de referencia
+         * 3- El tercer toque continuara a la siguiente etapa
+         */
+        if(pp_num==2){SaveImage();}
+        if(pp_num==1){CONTOUR_COLOR = new Scalar(0,255,0,255);pp_num++;}
+        if(pp_num==0){pp_num++; pp_mRgba_original=mRgba.clone(); }
+
+
+        int cols = mRgba.cols();
+        int rows = mRgba.rows();
+
+        int xOffset = (mOpenCvCameraView.getWidth() - cols) / 2;
+        int yOffset = (mOpenCvCameraView.getHeight() - rows) / 2;
+
+        int x = (int)event.getX() - xOffset;
+        int y = (int)event.getY() - yOffset;
+
+        Log.i(TAG, "Touch image coordinates: (" + x + ", " + y + ")");
+
+        if ((x < 0) || (y < 0) || (x > cols) || (y > rows)) return false;
+
+        Rect touchedRect = new Rect();
+
+        touchedRect.x = (x>4) ? x-4 : 0;
+        touchedRect.y = (y>4) ? y-4 : 0;
+
+        touchedRect.width = (x+4 < cols) ? x + 4 - touchedRect.x : cols - touchedRect.x;
+        touchedRect.height = (y+4 < rows) ? y + 4 - touchedRect.y : rows - touchedRect.y;
+
+        Mat touchedRegionRgba = mRgba.submat(touchedRect);
+
+        Mat touchedRegionHsv = new Mat();
+        Imgproc.cvtColor(touchedRegionRgba, touchedRegionHsv, Imgproc.COLOR_RGB2HSV_FULL);
+
+        // Calculate average color of touched region
+        mBlobColorHsv = Core.sumElems(touchedRegionHsv);
+        int pointCount = touchedRect.width*touchedRect.height;
+        for (int i = 0; i < mBlobColorHsv.val.length; i++)
+            mBlobColorHsv.val[i] /= pointCount;
+
+        mBlobColorRgba = converScalarHsv2Rgba(mBlobColorHsv);
+
+        Log.i(TAG, "Touched rgba color: (" + mBlobColorRgba.val[0] + ", " + mBlobColorRgba.val[1] +
+                ", " + mBlobColorRgba.val[2] + ", " + mBlobColorRgba.val[3] + ")");
+
+        mDetector.setHsvColor(mBlobColorHsv);
+
+        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE);
+
+        mIsColorSelected = true;
+
+        touchedRegionRgba.release();
+        touchedRegionHsv.release();
+
+        return false; // don't need subsequent touch events
+    }
+
+    public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
+        /**
+         * PP NCH  23/07/2017
+         * La funcion se modifico para que al haber seleccionado el color con onTouch()
+         * no actualize el frame que se muestra en el OpenCvCameraView
+         * Despues de seleccionar el color la imagen se mantiene igual, posteriormente se imprimen los contornos
+         * seleccionados  y como no cambia la imagen no se agregan mas contornos
+         */
+        if(pp_num==2){}
+        else {
+            if (mIsColorSelected) {
+                Log.d(TAG, "onCameraFrame(miscolorselected): Done 1");
+                if (mRgba.empty()) {
+                    Log.d(TAG, "onCameraFrame: Crash");
+                    exit(0);
+                }
+                /**
+                 * TODO Checa esto richi, hay que revisar por que la variable mRgba se vacia
+                 * DONE
+                 * NLJS evento onCameraViewStoppped limpia la variable mRgba por eso al llegar a estoya no tenia nada
+                 */
+                mDetector.process(mRgba);
+                List<MatOfPoint> contours = mDetector.getContours();
+                Log.e(TAG, "Contours count: " + contours.size());
+
+                Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+                Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+                colorLabel.setTo(mBlobColorRgba);
+
+                Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+                mSpectrum.copyTo(spectrumLabel);
+
+            } else {
+                mRgba = inputFrame.rgba();
+            }
+        }
+        return mRgba;
+    }
+
+    private Scalar converScalarHsv2Rgba(Scalar hsvColor) {
+        Mat pointMatRgba = new Mat();
+        Mat pointMatHsv = new Mat(1, 1, CvType.CV_8UC3, hsvColor);
+        Imgproc.cvtColor(pointMatHsv, pointMatRgba, Imgproc.COLOR_HSV2RGB_FULL, 4);
+
+        return new Scalar(pointMatRgba.get(0, 0));
+    }
+
+    public void Act(View view) {
+
+        /**
+         * PP NCH 23/07/2017
+         * Genera un intend para abrir la actividad P
+         */
+
+        // PP NLJS 22/07/2017 Creo intent para una nueva Actividad
+        Intent intent =new Intent(this,P.class);
+
+        // PP NLJS 22/07/2017 Envio los siguientes datos como EXTRAS
+        // private Mat                  mRgba;
+        // private Scalar               mBlobColorHsv;
+        // private Scalar               mBlobColorRgba;
+        // private ColorBlobDetector    mDetector;
+        // private Mat                  mSpectrum;
+        // private Size                 SPECTRUM_SIZE;
+        // private Scalar               CONTOUR_COLOR;
+        // private String               pp_imgAdd;
+
+        intent.putExtra("PP_EXTRA_MAT",mRgba);
+        Log.d(TAG, "chargeFile2: width: "+mRgba.cols());
+        Log.d(TAG, "chargeFile2: rowa: "+mRgba.rows());
+        intent.putExtra("PP_EXTRA_SCALAR",mBlobColorHsv);
+        intent.putExtra("PP_EXTRA_SCALAR2",mBlobColorRgba);
+        intent.putExtra("PP_EXTRA_COLORBLOBDETECTOR",mDetector);
+        intent.putExtra("PP_EXTRA_MAT2",mSpectrum);
+        intent.putExtra("PP_EXTRA_SIZE",SPECTRUM_SIZE);
+        intent.putExtra("PP_EXTRA_SCALAR3",CONTOUR_COLOR);
+        intent.putExtra("PP_EXTRA_STRING",pp_imgAdd);
+
+        // PP NLJS 16/07/2017 Inicializo la actividad
+        startActivity(intent);
+    }
+
+    int pp_num=0;//PP NCH 23/07/2017 Identifica contador para Imagen Objeto de referencia y objeto a medir
+
+
+    public void SaveImage(){
+        /**
+         * PP NCH 23/07/2017
+         * Convierte el objeto Mat mRgba en un bitmap para asi poder guardarlo en un archivo PNG
+         * en el directorio /proportion
+         * y un numero que identifica que imagen es
+         *
+         *  1 - Imagen Original
+         *  2 - Imagen Objeto a medir
+         *  3 - Imagen Objeto de referencia
+         *
+         *  PP NCH 30/07/2017
+         *  Solo guarda la imagen final con la seleccion de los dos objetos  si requiere repetir se hara el de los tres procesos
+         */
+
+        //PP NCH 23/07/2017 Creo bitmap temporal para poder guardar Mat mRgba en un archivo
+        Bitmap pp_bmp = null;
+        Bitmap pp_bmp_ori = null;
+
+
+        //PP NLJS 06/08/2017 Intento inicializar el Bitmap
+        try {
+            //PP NLJS 06/08/2017 Establezco el tamaño del Bitmap
+            pp_bmp = Bitmap.createBitmap(mRgba.cols(), mRgba.rows(), Bitmap.Config.ARGB_8888);
+            pp_bmp_ori = Bitmap.createBitmap(pp_mRgba_original.cols(), pp_mRgba_original.rows(), Bitmap.Config.ARGB_8888);
+            //PP NLJS 06/08/2017 Realizo la conversión
+            Utils.matToBitmap(mRgba, pp_bmp);
+            Utils.matToBitmap(pp_mRgba_original, pp_bmp_ori);
+
+        } catch (CvException e) {
+            Log.d(TAG, e.getMessage());
+            Log.d(TAG, "PP Función: SaveImage. No fué posible transformar mRgba a Bitmap.");
+            exit(0);
+        }
+        //PP NLJS 06/08/2017 Libero el espacio ocupado por mRgba
+        //mRgba.release();
+
+        //PP NLJS 06/08/2017 Reviso si el almacenamiento externo esta disponible para guardar la imagen
+        boolean pp_flag = isExternalStorageWritable();
+        Log.d(TAG, "PP Función: SaveImage. Validación de almacenamiento externo: " + pp_flag);
+
+        //PP NLJS 06/08/2017 Creo el File donde guardar la imagen y el directorio
+        File pp_img = getAlbumStorageDir("Proportion");
+
+        //PP NLJS 06/08/2017 Válido que pp_img no sea nulo
+        if(pp_img.exists()){
+            //PP NLJS 13/08/2017 Cuento cuantas fotos hay en la carpeta de Proportion para asignarle un número a esta nueva img
+            String [] pp_Files = pp_img.list();
+            int pp_totFiles = pp_Files.length;
+            pp_totFiles=pp_totFiles/2;
+            String filename = "Proportion_" + pp_totFiles + ".png";
+            String filename2 = "Proportion_" + pp_totFiles + "_ori.png";
+            boolean pp_flag_comp = false;
+            boolean pp_flag_comp2 = false;
+
+            //PP NLJS 06/08/2017 Creo el FOS y File destino para guardar la img en la memoria del teléfono
+            FileOutputStream pp_out = null;
+            FileOutputStream pp_out2 = null;
+            File pp_img2 = new File(pp_img,filename);
+            File pp_img3 = new File(pp_img,filename2);
+
+            //PP NLJS 06/08/2017 Intento inicializar el FOS
+            try {
+                pp_out = new FileOutputStream(pp_img2);
+                pp_out2 = new FileOutputStream(pp_img3);
+                pp_flag_comp = pp_bmp.compress(Bitmap.CompressFormat.PNG, 100, pp_out); // pp_bmp is your Bitmap instance
+                pp_flag_comp2 = pp_bmp_ori.compress(Bitmap.CompressFormat.PNG, 100, pp_out2); // pp_bmp is your Bitmap instance
+                pp_out.close();
+                pp_out.close();
+
+                // PP NLJS 13/08/2017 Guardo la dirección de la foto que se acaba de guardar
+                pp_imgAdd = pp_img2.getAbsolutePath();
+                pp_imgAdd2 = pp_img3.getAbsolutePath();
+                Log.d(TAG, "PP Función: SaveImage. La imagen se guardó exitosamente. pp_imgAdd : " + pp_imgAdd);
+
+                if (mOpenCvCameraView != null) {
+                    Mat lol= mRgba.clone();
+                    mOpenCvCameraView.disableView();
+                    mRgba=lol.clone();
+                    Act(mOpenCvCameraView);
+
+                }else {
+                    Act(mOpenCvCameraView);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                Log.d(TAG, "PP Función: SaveImage. Error al crear el FOS. Compress : " + pp_flag_comp);
+            }
+        }
+    }
+    public boolean isExternalStorageWritable() {
+        /**
+         * PP NLJS 06/08/2017
+         * Revisa si el almacenamiento externo esta disponible para leer y escribir
+         *
+         */
+
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    public File getAlbumStorageDir(String albumName) {
+        /**
+         * PP NLJS 06/08/2017
+         * Crea el directorio donde se van a guardar la fotos (Este será público para otras aplicaciones)
+         *
+         */
+
+        // PP NLJS 06/08/2017 Creo el File que se va a utilizar y la ruta de almacenamiento
+        // PP NLJS 06/08/2017 DIRECTORI_PICTURE Es un parametro necesario para indicar que el archivo a guardar será una imagen y la ruta se genere bien
+        File pp_file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), albumName);
+
+        // PP NLJS 06/08/2017 Creo el directorio y mando un msg de error en caso de que ya exista o no se haya podido crear
+        if (!pp_file.mkdirs()) {
+            Log.e(TAG, "PP Función: getAlbumStorageDir. El directorio ya existía o no se pudo crear. " + pp_file.getAbsolutePath());
+        }
+        return pp_file;
+    }
+}
---
+++ color-blob-detection-verAlt-3.3.0/openCVSamplecolorblobdetection/src/main/java/org/opencv/samples/colorblobdetect/P.java
@@ -0,0 +1,275 @@
+package org.opencv.samples.colorblobdetect;
+
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.net.Uri;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import org.opencv.android.Utils;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.core.Scalar;
+import org.opencv.core.Size;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.File;
+import java.util.List;
+
+public class P extends Activity {
+
+    private static final String  TAG = "P::Activity";
+
+    private static int RESULT_LOAD_IMAGE = 1;
+    private Button botonAbrir;
+    private Button botonConfirmar;
+
+    // PP NLJS 13/08/2017 Creo variables necesarias para cargar la imagen
+    private ImageView imageView;
+    private ImageView imV;
+    private Bitmap loadedImage;
+
+    // PP NLJS 13/08/2017 Creo variables que recibirá de la actividad que lo mando a llamar
+    private String               pp_imgAdd;
+
+    private Scalar               CONTOUR_COLOR;
+    private Mat                  mRgba;
+    private Scalar               mBlobColorHsv;
+    private ColorBlobDetector    mDetector;
+    private Scalar               mBlobColorRgba;
+    private Mat                  mSpectrum;
+    private Size                 SPECTRUM_SIZE;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_p);
+
+        // PP NLJS 15/07/2017 Obtengo la información del intend que lo mando a llamar
+        Intent intent = getIntent();
+
+        mBlobColorRgba = new Scalar(255);
+        mDetector = new ColorBlobDetector();
+
+        // PP NLJS 15/07/2017 Obtengo los siguientes EXTRAS del intend que lo mando a llamar
+        // private Mat                  mRgba;
+        // private Scalar               mBlobColorHsv;
+        // private Scalar               mBlobColorRgba;
+        // private ColorBlobDetector    mDetector;
+        // private Mat                  mSpectrum;
+        // private Size                 SPECTRUM_SIZE;
+        // private Scalar               CONTOUR_COLOR;
+        // private String               pp_imgAdd;
+
+        mRgba = (Mat)getIntent().getExtras().getSerializable("PP_EXTRA_MAT");
+        Log.d(TAG, "chargeFile2: width1: "+mRgba.cols());
+        mBlobColorHsv = (Scalar)getIntent().getExtras().getSerializable("PP_EXTRA_SCALAR");
+        mBlobColorRgba = (Scalar)getIntent().getExtras().getSerializable("PP_EXTRA_SCALAR2");
+        mDetector = (ColorBlobDetector) getIntent().getExtras().getSerializable("PP_EXTRA_COLORBLOBDETECTOR");
+        mSpectrum = (Mat) getIntent().getExtras().getSerializable("PP_EXTRA_MAT2");
+        SPECTRUM_SIZE = (Size) getIntent().getExtras().getSerializable("PP_EXTRA_SIZE");
+        CONTOUR_COLOR = (Scalar) getIntent().getExtras().getSerializable("PP_EXTRA_SCALAR3");
+
+        pp_imgAdd = intent.getStringExtra("PP_EXTRA_STRING");
+        Log.d(TAG, "PP Función: (Activity: P) onCreate. pp_imgAdd : " + pp_imgAdd);
+        // PP NLJS 13/08/2017
+        imageView = (ImageView) findViewById(R.id.test_image);
+        Log.d(TAG, "chargeFile2: width1_1: "+mRgba.cols());
+        // PP NLJS 13/08/2017 Abro la imagen en el ImageView
+        chargeFile(pp_imgAdd);
+        Log.d(TAG, "chargeFile2: width1_2: "+mRgba.cols());
+        //chargeFile2();
+        Log.d(TAG, "chargeFile2: width1_3: "+mRgba.cols());
+
+        // PP NLJS 15/07/2017 Obtengo los valores RGB
+        /*mBlobColorRgba.val[0] = intent.getDoubleExtra(ColorBlobDetectionActivity.EXTRA_RED,0.0);
+        mBlobColorRgba.val[1] = intent.getDoubleExtra(ColorBlobDetectionActivity.EXTRA_GREEN,0.0);
+        mBlobColorRgba.val[2] = intent.getDoubleExtra(ColorBlobDetectionActivity.EXTRA_BLUE,0.0);
+        */
+
+        // PP NLJS 15/07/2017 Imprimo valores RGB, solo como prueba en consola
+        /*Log.d(TAG, "Extra rojo: " + mBlobColorRgba.val[0]);
+        Log.d(TAG, "Extra verde: " + mBlobColorRgba.val[1]);
+        Log.d(TAG, "Extra azul: " + mBlobColorRgba.val[2]);
+
+
+        mBlobColorRgba.val[3]= (double) 255;
+        */
+
+        imageView = (ImageView) findViewById(R.id.test_image);
+        botonAbrir = (Button) findViewById(R.id.botonAbrirImagen);
+        botonConfirmar = (Button) findViewById(R.id.botonConfirmar);
+
+        botonConfirmar.setEnabled(false);
+
+        CONTOUR_COLOR = new Scalar(255,0,0,255);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            ImageView imageView = (ImageView) findViewById(R.id.test_image);
+            Button botonConfirmar = (Button) findViewById(R.id.botonConfirmar);
+            Bitmap imagen = getImage(data);
+            if ( imagen != null ){
+                imageView.setImageBitmap(imagen);
+                botonConfirmar.setEnabled(true);
+                //postproceso();
+            }else{
+                Log.i("Hola", "La imagen no existe");
+            }
+
+        }
+    }
+
+    public void actionBotonConfirmar(View view){
+        AlertDialog.Builder alert = new AlertDialog.Builder(P.this);
+        alert.setTitle("Aceptar");
+        alert.setMessage("Quieres usar esta imagen?");
+        alert.setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //ir al resultado
+            }
+        });
+        alert.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Regresar a seleccionar imagen
+            }
+        });
+        alert.show();
+    }
+
+    public void actionBotonAbrirImagen(View view){
+        Intent loadIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        startActivityForResult(loadIntent, RESULT_LOAD_IMAGE);
+    }
+
+    public Bitmap getImage(Intent data){
+        Uri selectedImage = data.getData();
+        String[] filePathColumn = { MediaStore.Images.Media.DATA };
+        Cursor cursor = getContentResolver().query(selectedImage, filePathColumn, null, null, null);
+        cursor.moveToFirst();
+
+        int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+        String picturePath = cursor.getString(columnIndex);
+        cursor.close();
+        File imgFile = new  File(picturePath);
+        if(imgFile.exists()){
+            //Convertir a bitmap desde direccion de la imagen
+            Bitmap myBitmap = BitmapFactory.decodeFile(imgFile.getAbsolutePath());
+            mRgba = new Mat(myBitmap.getHeight(), myBitmap.getWidth(), CvType.CV_8UC1);
+            //postproceso();
+            //Rotar Imagen
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap imagenRotada = Bitmap.createBitmap(myBitmap , 0, 0, myBitmap .getWidth(), myBitmap.getHeight(), matrix, true);
+            return imagenRotada;
+        }
+        return null;
+
+    }
+
+    public void postproceso(){
+
+        /**
+         * TODO: Arreglar el error que crashea la app
+         */
+
+        mDetector.setHsvColor(mBlobColorHsv);
+        Log.d(TAG, "postproceso: Still working 1");
+
+        mDetector.process(mRgba);//aqui mueres
+        Log.d(TAG, "postproceso: Still working 2");
+        List<MatOfPoint> contours = mDetector.getContours();
+        Log.e(TAG, "Contours count: " + contours.size());
+
+        Imgproc.drawContours(mRgba, contours, -1, CONTOUR_COLOR);
+
+        Mat colorLabel = mRgba.submat(4, 68, 4, 68);
+        colorLabel.setTo(mBlobColorRgba);
+
+
+        Bitmap img = Bitmap.createBitmap(mRgba.cols(), mRgba.rows(),Bitmap.Config.ARGB_8888);
+
+        /*
+        mBlobColorHsv= converScalarRgba2Hsv(mBlobColorRgba);
+
+        mDetector.setHsvColor(mBlobColorHsv);
+
+        Imgproc.resize(mDetector.getSpectrum(), mSpectrum, SPECTRUM_SIZE);
+
+        Mat spectrumLabel = mRgba.submat(4, 4 + mSpectrum.rows(), 70, 70 + mSpectrum.cols());
+        mSpectrum.copyTo(spectrumLabel);*/
+
+
+    }
+
+
+
+
+    void chargeFile(String imgAdd) {
+        /**
+         * PP NLJS 13/08/2017
+         * Carga la imagen original en em imageView principal
+         *
+         */
+        Log.d(TAG, "chargeFile2: width4: "+mRgba.cols());
+        try {
+            BitmapFactory.Options options = new BitmapFactory.Options();
+            Log.d(TAG, "chargeFile2: width4_1: "+mRgba.cols());
+            options.inPreferredConfig = Bitmap.Config.ARGB_8888;
+            Log.d(TAG, "chargeFile2: width4_2: "+mRgba.cols());
+            Mat temp=mRgba.clone();
+            Bitmap pp_bitmap = BitmapFactory.decodeFile(imgAdd, options);
+            mRgba=temp.clone();
+            Log.d(TAG, "chargeFile2: width4_3: "+mRgba.cols()+"    Height: "+mRgba.rows());
+
+
+            //Rotar Imagen
+            Matrix matrix = new Matrix();
+            matrix.postRotate(90);
+            Bitmap imagenRotada = Bitmap.createBitmap(pp_bitmap , 0, 0, pp_bitmap .getWidth(), pp_bitmap.getHeight(), matrix, true);
+
+
+            imageView.setImageBitmap(imagenRotada);
+
+
+        }catch (Exception e){
+            // PP NLJS 13/08/2017 Mando mensaje de error en caso de que la img no se pudiera cargar en el ImageView
+            Log.d(TAG, "PP Función: (Activity: P) chargeImage. No se pudo cargar la imagen: "+ e.getMessage());
+        }
+    }
+
+    //PP NCH 13/08/2017
+    void chargeFile2(){
+        Log.d(TAG, "chargeFile2: width2: "+mRgba.cols());
+        try{
+            Log.d(TAG, "chargeFile2: width3: "+mRgba.cols());
+            Bitmap pp_bitmap2 = Bitmap.createBitmap(mRgba.cols(), mRgba.rows(),Bitmap.Config.ARGB_8888);
+            Utils.matToBitmap(mRgba,pp_bitmap2);
+            imV.setImageBitmap(pp_bitmap2);
+        }catch (Exception e){
+            Log.d(TAG, "chargeFile2: Error: "+e.getMessage());
+        }
+
+
+    }
+
+
+}
